7f7db61e7f2820e849a58d0382b27ce2a70dad2e
Shruti Vij <67600604+shrutiVij1502@users.noreply.github.com>
2022-12-13 13:18:56 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "Installing source NPM dependencies..."
-#npm install
-#node /home/ec2-user/app.js
-pm2 start /home/ec2-user/app.js 
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum install -y nodejs
+sudo yum -y install gcc-c++ make
+node --version
+sudo npm install pm2 -g -y
+#pm2 start /home/ec2-user/app.js 
 
